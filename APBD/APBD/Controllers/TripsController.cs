ba191835f3a731c134253d1300671ec55bfead4f
Anna Okon <s27684@pjwstk.edu.pl>
2024-05-28 11:18:54 +0200

    Commit

---
+++ APBD/APBD/Controllers/TripsController.cs
@@ -0,0 +1,32 @@
+﻿using APBD.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APBD.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TripsController : ControllerBase
+{
+    private readonly ApbdContext _context;
+
+    public TripsController(ApbdContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+        var trips = await _context.Trips.Select(e => new
+        {
+            Name = e.Name,
+            Countries = e.IdCountries.Select(c => new
+            {
+                Name = c.Name
+            })
+        }).ToListAsync();
+        
+        return Ok(trips);
+    }
+}
---
+++ APBD/APBD/Models/DTOs/CountryDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace APBD.Models.DTOs;
+
+public class CountryDTO
+{
+    
+}
